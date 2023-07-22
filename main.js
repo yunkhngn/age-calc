7d6f08c91ff758bcfb00b50a031a389b32566e17
yunkhngn <rogkhoa.mail@gmail.com>
2023-07-22 13:15:47 +0700

    Finishing functions

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ electronReload(__dirname);
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 600,
-    height: 400,
+    width: 500,
+    height: 375,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     },
